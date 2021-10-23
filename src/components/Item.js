937b6545a74df546d7a9a8f260fa5d75ed819806
aurionhoward <aurionhoward@gmail.com>
2021-10-22 21:06:58 -0400

    Completed dark-mode

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,30 @@
 import React from "react";
+// import {items} from "../data/items.js";
+import { useState } from 'react'
+
 
 function Item({ name, category }) {
+  const [addItem, setAddedItems] = useState(true)
+  
+
+
+  function handleClick({items}) {
+ 
+  setAddedItems((addItem) => !addItem)
+
+  setCart((inCart) => !inCart)
+  }
+
+
+const [inCart, setCart] = useState(true)
+
+  
+
   return (
-    <li className="">
+    <li className={inCart ? "" : "in-cart"} >
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className="add">{!addItem ? "Add to Cart" : "Remove From Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Item from "./Item";
+import {useState} from "react"
 
-function ShoppingList({ items }) {
+function ShoppingList({items} ) {
+
+  const [selectedCategory, setCategory] = useState(items)
+  console.log(items)
+
+  function handleFilter(e) {
+   const itemsFiltered = selectedCategory.filter ( category => {
+        return category.category(e.target.value)
+      })
+      setCategory(itemsFiltered)
+   
+    }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onClick={handleFilter}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
